4735b4443aca14f4c0ebb282f0f11cbd85877008
MagicIrfan <irfan.bouhenaf@etu.mines-ales.fr>
2023-08-19 22:45:03 +0200

    petite reorganisation

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/stylesheets/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Gros nibards</h1>
+    </div>
+  );
+}
+
+export default App;
