baeba2d41daf856e647f1306d4dbad2ece82aab4
MagicIrfan <irfan.bouhenaf@etu.mines-ales.fr>
2023-08-19 23:28:36 +0200

    create search block

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import '../assets/stylesheets/App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 
-function App() {
+const App : React.FC = () => {
   return (
     <div className="App">
-      <h1>Gros nibards</h1>
+      <h1>Pokemons</h1>
+        <div className={"search"}>
+          <FontAwesomeIcon icon={icon({name: 'magnifying-glass', style:'solid'})} />
+          <input className={"search-poke"} type={"text"} placeholder={"Search a pokémon !"}/>
+        </div>
     </div>
   );
 }
