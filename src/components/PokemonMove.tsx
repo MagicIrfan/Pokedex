28ec2a923c7913dbf5e9d24f06a4eeb4ee850d96
MagicIrfan <irfan.bouhenaf@etu.mines-ales.fr>
2024-03-24 15:44:56 +0100

    continuation refacto

--- src/components/PokemonMove.tsx
+++ src/components/PokemonMove.tsx
@@ -3,23 +3,35 @@ import {PokemonMoveCharacteristic} from "./PokemonMoveCharacteristic";
 import {PokemonMoveTypeSVG} from "./PokemonMoveTypeSVG";
 import {PokemonMove as PokemonMoveModel} from "../models/PokemonMove";
 import {prettier} from "../utils/string";
+import physical from "../assets/images/physical.webp";
+import special from "../assets/images/special.webp";
+import status from "../assets/images/status.webp";
 
 interface PokemonMoveProps{
     move:PokemonMoveModel;
 }
 
 export const PokemonMove : React.FC<PokemonMoveProps> = ({move}) => {
+    const moveType = {
+        physical:physical,
+        special:special,
+        status:status
+    }
     return (
         <div className={`pokemon-move ${move.pokemonType}`}>
-            <p className={"pokemon-move-name"}>{prettier(move.name)}</p>
-            <p>{prettier(move.description)}</p>
+            <h2 className={"pokemon-move-name"}>{prettier(move.name)}</h2>
+            <p style={
+                {
+                    fontSize:1.2 + 'em'
+                }
+            }>{prettier(move.description)}</p>
             <div className={"pokemon-move-bottom"}>
                 <div className={"pokemon-move-characteristics"}>
-                    <PokemonMoveCharacteristic name={"Power"} value={move.power}/>
-                    <PokemonMoveCharacteristic name={"Accuracy"} value={move.accuracy}/>
+                    {move.power && <PokemonMoveCharacteristic name={"Power"} value={move.power}/>}
+                    {move.accuracy && <PokemonMoveCharacteristic name={"Accuracy"} value={move.accuracy}/>}
                     <PokemonMoveCharacteristic name={"PP"} value={move.pp}/>
-                    <PokemonMoveCharacteristic name={"Priority"} value={move.priority}/>
-                    <PokemonMoveCharacteristic name={"Type"} value={prettier(move.moveType)}/>
+                    {move.priority !== 0 && <PokemonMoveCharacteristic name={"Priority"} value={move.priority}/>}
+                    <PokemonMoveCharacteristic image={moveType[move.moveType as keyof typeof moveType]}/>
                     <PokemonMoveCharacteristic name={"Target"} value={prettier(move.target)}/>
                 </div>
                 <PokemonMoveTypeSVG type={move.pokemonType}/>
