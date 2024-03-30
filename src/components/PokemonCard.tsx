9ed3bc0e0799ce10fd218ab757a8b4b65f05e7ca
MagicIrfan <irfan.bouhenaf@etu.mines-ales.fr>
2024-03-30 20:42:44 +0100

    refacto

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -12,19 +12,33 @@ export interface PokemonProps {
 
 const PokemonComponent: React.FC<PokemonProps> = ({ pokemon }) => {
     const { types, id, name } = pokemon;
-    const navigate : NavigateFunction = useNavigate();
-    const onClickPokemon = (id : number) : void => {
+    const navigate: NavigateFunction = useNavigate();
+
+    const onClickPokemon = (id: number): void => {
         navigate(`/${id}`);
-    }
+    };
+
+    const onKeyPress = (event: React.KeyboardEvent, id: number): void => {
+        if (event.key === 'Enter' || event.key === ' ') {
+            onClickPokemon(id);
+        }
+    };
 
     return (
-        <div className={"pokemon " + (types[0])} onClick={() => onClickPokemon(id)}>
+        <div
+            className={"pokemon " + (types[0])}
+            onClick={() => onClickPokemon(id)}
+            onKeyPress={(event) => onKeyPress(event, id)}
+            tabIndex={0}
+            role="button"
+            style={{ cursor: 'pointer' }}
+        >
             <p className="poke-number">{pokeNumber(id)}</p>
             <h2>{capitalize(name)}</h2>
             <div className="poke-body">
                 <PokemonTypes types={types}/>
                 <div className="poke-image">
-                    <Image className={"image-poke"} src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`} width={160} height={160} alt="pokemon" />
+                    <Image className={"image-poke"} src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`} width={160} height={160} alt={`pokemon ${name}`} />
                     <Image className={"pokeball-img"} src={pokeball} width={200} height={200} alt="pokeball" />
                 </div>
             </div>
